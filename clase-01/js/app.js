37683e6caf3f0ec10b53e09a06806dddb17bb514
svalenciag95 <101234659+svalenciag95@users.noreply.github.com>
2023-06-22 08:21:43 -0500

    Add files via upload

---
+++ clase-01/js/app.js
@@ -0,0 +1,9 @@
+console.log("un valor de tipo texto");
+console.log(20);
+console.log(1.2);
+console.log(1, 62);
+console.log(1 + 1);
+console.log(true);
+console.log(false);
+
+
---
+++ clase-02/js/app.js
@@ -0,0 +1,75 @@
+// JavaScript tiene tres tipos de declaraciones de variables.
+
+// console.log("conectados existosamente")
+
+// let x = 20;
+// let y = 20;
+
+// let resultado = x * y;
+
+// console.log(resultado);
+
+// intentar crear las variables (con datos inventados) para que este script funcion
+
+let nombre = "sebastian";
+
+console.log("Su nombre es:" +nombre);
+
+let nombreUsuario = "svalenciag95";
+
+console.log("su nombre de usario es" +nombreUsuario);
+
+let edad = 26;
+
+console.log("Su edad es:" +edad);
+console.log(edad);
+
+let nacionalidad = "Colombiano"
+console.log("Su nacionalidad es:" +nacionalidad);
+console.log(nacionalidad);
+
+let estadoCivl = "soltero"
+console.log("¿está comprometido?" +estadoCivl);
+console.log(estadoCivl);
+
+//concatenacion "+"
+
+let one = "Hello, ";
+let two ="How are you?";
+let joined = one + two;
+
+console.log(joined);
+
+console.log(one + two);
+
+console.log("Hello, " + "how are you?");
+
+//mini practica
+
+console.log("Su nombre de usuario    es: " + nombreUsuario + " y su edad es: " + edad);
+
+//que pasaria en la concateanicion con los numeros??
+
+// let numeroUno = 100;
+// let numeroDos = 200;
+
+// console.log(numeroUno + numeroDos);  
+
+//promt
+
+// let apellido = prompt ("Ingresa un apellido: ");
+// console.log(apellido);
+
+let numeroUno = prompt("ingrese el primero numero: ");
+let numeroDos = prompt("ingrese el segundo numero: ")
+
+
+//parseInt convierte de cadena String intero
+let resultado = parseInt(numeroUno) + parseInt(numeroDos);  
+console.log(resultado)
+
+//Operadores
+
+// Operadores Aritméticos o Algebraicos o Matemáticos.
+// Operadores de Comparación / Relacionales.
+// Operadores lógicos.
